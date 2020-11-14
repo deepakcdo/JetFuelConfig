7251bbefbfdc3a643179ccdd0ec2491b62ea7c18
Deepak <deepakcdo@gmail.com>
2020-11-14 12:57:23 +0000

    Added Docker support

---
+++ scripts/clearLocalLogsAndJournals.sh
@@ -0,0 +1,3 @@
+
+rm -rf ../ampsFiles/LDN-PRIMARY/logs ampsFiles/LDN-PRIMARY/sow_d ampsFiles/LDN-PRIMARY/journal
+rm -rf ../ampsFiles/LDN-BACKUP/logs ampsFiles/LDN-BACKUP/sow_d ampsFiles/LDN-BACKUP/journal
--- scripts/startAmps.sh
+++ scripts/startAmps.sh
@@ -1,4 +1,4 @@
 cd /root/JetFuelConfig/
-current/bin/ampServer -DAMPS_NAME=LDN-PRIMARY config/LDN-Primary.xml &
-current/bin/ampServer -DAMPS_NAME=LDN-BACKUP config/LDN-Backup.xml &
+current/bin/ampServer -DAMPS_NAME=LDN-PRIMARY -DREP_SERVER=127.0.0.1 config/LDN-Primary.xml &
+current/bin/ampServer -DAMPS_NAME=LDN-BACKUP -DREP_SERVER=127.0.0.1 config/LDN-Backup.xml &
 
