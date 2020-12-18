09ca875f151e2963a81cb8bd4d5c6fc7eddbb3ec
Deepak D <deepakcdo@gmail.com>
2020-12-18 15:32:31 +0000

    Fixed minor script Issues with force deleting

--- scripts/installAmps.sh
+++ scripts/installAmps.sh
@@ -7,6 +7,6 @@ echo Installing $FULL_VERSION
 cd ../
 wget http://devnull.crankuptheamps.com/releases/amps/$VERSION/$FULL_VERSION.tar.gz
 tar -xvf $FULL_VERSION.tar.gz
-rm current
+rm -f current
 ln -s $FULL_VERSION current
 cd scripts
