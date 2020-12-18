d97c8ad4c993c1f03a5d5544c0955e9afe24f6dc
Deepak <deepakcdo@gmail.com>
2020-12-18 15:42:19 +0000

    Updated config

--- scripts/startAmps.sh
+++ scripts/startAmps.sh
@@ -1,4 +1,4 @@
-cd /root/JetFuelConfig/
+cd ../
 current/bin/ampServer -DAMPS_NAME=LDN-PRIMARY -DREP_SERVER=127.0.0.1 config/LDN-Primary.xml &
 current/bin/ampServer -DAMPS_NAME=LDN-BACKUP -DREP_SERVER=127.0.0.1 config/LDN-Backup.xml &
 
